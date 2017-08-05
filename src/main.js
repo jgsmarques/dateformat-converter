c4aaa6a402374e92cae68fb77ccb4c467c51dd3b
Joao Marques <jgmarques@kpmg.com>
2017-08-05 16:49:47 +0100

    feat(main): check that all properties from source configuration exist in destination
    if any property does not exist in the destination configuration but exists in the source
    configuration, it is copied over, to ensure that the pattern keeps its original value

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Converter from './Converter';
 
 export default function(valueToConvert, sourceFormat, destinationFormat) {
+	for (const prop in sourceFormat) {
+		if (!(prop in destinationFormat)) {
+			destinationFormat[prop] = sourceFormat[prop];
+		}
+	}
 	return Converter.format(Converter.parse(valueToConvert, sourceFormat), destinationFormat);
 }
